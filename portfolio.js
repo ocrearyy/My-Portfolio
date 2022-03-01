2a7b04077ba43e66b4bed0faf4bbd441bfd70067
Oshane Design <oshane.design@gmail.com>
2022-03-01 09:51:22 -0500

    Added Javascript class selectors

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,9 @@
-js
+const appearMenu = document.querySelector('.menuDropDown')
+const disappearMenu = document.querySelector('.x-icon');
+const disappearMenuOptions = document.querySelectorAll('.mobile-menu-li');
+const displaySelectedPage = document.querySelectorAll('.mobile-menu-li');
+
+appearMenu.addEventListener('click', function() {  });
+disappearMenu.addEventListener('click', function() {  })
+disappearMenuOptions.addEventListener('click', function() {  })
+displaySelectedPage.addEventListener('click', function() {  })
