1c5cc4c82c2b84db7038835a0f07e000fbaee49d
Oshane Design <oshane.design@gmail.com>
2022-03-01 10:43:36 -0500

    Fixed eslint errors

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,16 @@
-const appearMenu = document.querySelector('.menuDropDown')
+const appearMenu = document.querySelector('.menuDropDown');
 const mobileMenu = document.querySelector('.default');
 const disappearMenu = document.querySelector('.x-icon');
 const disappearMenuOptions = document.querySelectorAll('.mobile-menu-li');
 
 function display() {
-    mobileMenu.classList.toggle('mobile-menu');
+  mobileMenu.classList.toggle('mobile-menu');
 }
 
 appearMenu.addEventListener('click', display);
 
 disappearMenu.addEventListener('click', display);
 
-for(let i = 0; i < disappearMenuOptions.length; i += 1) {
-disappearMenuOptions[i].addEventListener('click', display);
+for (let i = 0; i < disappearMenuOptions.length; i += 1) {
+  disappearMenuOptions[i].addEventListener('click', display);
 }
