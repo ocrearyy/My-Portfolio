98109e13d88a8156ed4a9478ca2ef35e1e9a63ad
Oshane Design <oshane.design@gmail.com>
2022-03-01 19:46:44 -0500

    Stored data in javascript object

--- portfolio.js
+++ portfolio.js
@@ -18,13 +18,50 @@ function display2() {
 for (let i = 0; i < disappearMenuOptions.length; i += 1) {
   disappearMenuOptions[i].addEventListener('click', display);
 }
-for (let j=0; j < popupClick.length; j += 1) {
+for (let j = 0; j < popupClick.length; j += 1) {
   popupClick[j].addEventListener('click', display2);
- }
+}
 
 appearMenu.addEventListener('click', display);
 disappearMenu.addEventListener('click', display);
 popupClose.addEventListener('click', display2);
 
-
+let portfolio = [
+  object1= {
+    img: 'images/Snapshoot-Portfolio1.svg',
+    name: 'Tonic',
+    information: ['CANOPY', 'Back End Dev', 2015],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technolgies: ['html', 'css', 'javascript'],
+    seeLive: 'https://oshanedesign.github.io/My-Portfolio/',
+    seeSource: 'https://github.com/oshanedesign/My-Portfolio',
+  },
+  object2= {
+    img: 'images/Snapshoot-Portfolio2.svg',
+    name: 'Tonic',
+    information: ['CANOPY', 'Back End Dev', 2015],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technolgies: ['html', 'css', 'javascript'],
+    seeLive: 'https://oshanedesign.github.io/My-Portfolio/',
+    seeSource: 'https://github.com/oshanedesign/My-Portfolio',
+  },
+  object3= {
+    img: 'images/Snapshoot-Portfolio3.svg',
+    name: 'Tonic',
+    information: ['CANOPY', 'Back End Dev', 2015],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technolgies: ['html', 'css', 'javascript'],
+    seeLive: 'https://oshanedesign.github.io/My-Portfolio/',
+    seeSource: 'https://github.com/oshanedesign/My-Portfolio',
+  },
+  object4= {
+    img: 'images/Snapshoot-Portfolio4.svg',
+    name: 'Tonic',
+    information: ['CANOPY', 'Back End Dev', 2015],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technolgies: ['html', 'css', 'javascript'],
+    seeLive: 'https://oshanedesign.github.io/My-Portfolio/',
+    seeSource: 'https://github.com/oshanedesign/My-Portfolio',
+  },
+];
 
